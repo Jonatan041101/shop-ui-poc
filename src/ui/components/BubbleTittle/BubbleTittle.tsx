0e16390b35065d56595fb51428d033a8fb9135e0
Jonatan <valdiviezojonatan.2101@gmail.com>
2024-03-31 17:31:09 -0300

    Update BubbleTittle component

--- src/ui/components/BubbleTittle/BubbleTittle.tsx
+++ src/ui/components/BubbleTittle/BubbleTittle.tsx
@@ -8,10 +8,12 @@ export default function BubbleTittle({
   className,
 }: IBubbleTittleProps): JSX.Element {
   return (
-    <div className={`absolute ${className}`}>
+    <div
+      className={`absolute transition-opacity opacity-0 group-hover:opacity-100 ${className}`}
+    >
       <div className="flex items-center justify-center rounded-md relative">
-        <div className="h-4 bg-red w-4 bg-black rotate-45 rounded-[4px] absolute left-[-6px]"></div>
-        <span className="text-white text-xs text-center bg-black p-2 rounded-md relative z-10">
+        <div className="h-4 bg-red w-4 bg-[#454545] rotate-45 rounded-[4px] absolute left-[-6px]"></div>
+        <span className="text-white text-xs text-center bg-[#454545] p-2 rounded-md relative z-10">
           {name}
         </span>
       </div>
