267d33d7dab0ff4a4e9349eb18be746c5e19424d
Jonatan <valdiviezojonatan.2101@gmail.com>
2024-03-31 17:30:04 -0300

    Update Navbar component

--- src/ui/components/Aside/domain/constants/navbar-constants.ts
+++ src/ui/components/Aside/domain/constants/navbar-constants.ts
@@ -1,7 +1,8 @@
+import { assetsStatics } from "@/ui/assets/statics";
 import { ILink } from "../interfaces/navbar-interface";
 
 export const links: ILink[] = [
-  { id: 1, name: "Products", href: "/products" },
-  { id: 2, name: "History", href: "/history" },
-  { id: 3, name: "Graphics", href: "/graphics" },
+  { id: 1, name: "Products", href: "/products", icon: assetsStatics.Products },
+  { id: 2, name: "History", href: "/history", icon: assetsStatics.History },
+  { id: 3, name: "Graph", href: "/graph", icon: assetsStatics.Graph },
 ];
