24581edcbb8fe6d08f73a78ba50b67ab92c2c334
Jonatan <valdiviezojonatan.2101@gmail.com>
2024-03-31 17:29:19 -0300

    Add assets

--- src/ui/assets/statics/index.ts
+++ src/ui/assets/statics/index.ts
@@ -1,16 +1,28 @@
 import Bottle from "./bottle.svg";
 import Cart from "./cart.svg";
 import Logo from "./logo.svg";
+import Graph from "./graph.svg";
+import History from "./history.svg";
+import Products from "./products.svg";
+import ProductVoid from "./product-void.svg";
 import { StaticImageData } from "next/image";
 
 enum AssetsStatics {
   Bottle = "Bottle",
   Cart = "Cart",
   Logo = "Logo",
+  Graph = "Graph",
+  History = "History",
+  Products = "Products",
+  ProductVoid = "ProductVoid",
 }
 
 export const assetsStatics: Record<AssetsStatics, StaticImageData> = {
   Bottle,
   Cart,
   Logo,
+  Graph,
+  History,
+  Products,
+  ProductVoid,
 };
