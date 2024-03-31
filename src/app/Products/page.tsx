f74240154255476cea967ac332277745151265df
Jonatan <valdiviezojonatan.2101@gmail.com>
2024-03-31 17:30:41 -0300

    Update Products component

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -1,11 +1,23 @@
 import ProductCard from "./components/Card";
 
-export default function ProductPage() {
+interface IProductPageProps {
+  username: string;
+}
+
+export default function ProductPage({ username }: IProductPageProps) {
   return (
-    <section>
+    <main className="bg-[#fafafe]">
+      <header>
+        <h1 className="text-2xl font-bold">
+          <span className="text-primary">Welcome! </span>
+          <span>{`We're delighted to have you with us, ${
+            username ?? "Jonatan"
+          }.`}</span>
+        </h1>
+      </header>
       <article>
         <ProductCard product={{ id: 1, name: "Jonatan" }} />
       </article>
-    </section>
+    </main>
   );
 }
